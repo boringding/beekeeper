6e953c22e46e98a524eb54a1de61bb4396ccacd0
dingyao(姚鼎) <dingyao@tencent.com>
2016-10-18 16:53:16 +0800

    create scripts

--- create.sh
+++ create.sh
@@ -1,1 +1,20 @@
 #!/bin/sh
+
+if [ ! $1 ]; then
+	echo "Usage: ./create.sh <project name>"
+	exit
+fi
+
+projName=$1
+
+cp -r ./skeleton $GOPATH/$projName
+
+cd $GOPATH/$projName/conf
+
+sed -i "s/beekeeper/$projName/g" framework.conf.dev.xml
+sed -i "s/beekeeper/$projName/g" framework.conf.test.xml
+sed -i "s/beekeeper/$projName/g" framework.conf.product.xml
+
+cd $GOPATH/$projName
+
+sed -i "s/github.com\/boringding\/beekeeper\/skeleton\/handlers/$projName\/handlers/g" main.go
