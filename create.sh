ecbdeefdd88a04d2e5c0ab56901d2f9f6e87367b
ddd <boring_ding@126.com>
2016-10-18 20:44:24 +0800

    create scripts

--- create.sh
+++ create.sh
@@ -7,14 +7,14 @@ fi
 
 projName=$1
 
-cp -r ./skeleton $GOPATH/$projName
+cp -r ./skeleton $GOPATH/src/$projName
 
-cd $GOPATH/$projName/conf
+cd $GOPATH/src/$projName/conf
 
 sed -i "s/beekeeper/$projName/g" framework.conf.dev.xml
 sed -i "s/beekeeper/$projName/g" framework.conf.test.xml
 sed -i "s/beekeeper/$projName/g" framework.conf.product.xml
 
-cd $GOPATH/$projName
+cd $GOPATH/src/$projName
 
 sed -i "s/github.com\/boringding\/beekeeper\/skeleton\/handlers/$projName\/handlers/g" main.go
